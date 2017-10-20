dca1b1805a6ca59a9a66434135a877cc85475fad
Mohd Maqbool Alam <maqbool@maqbool.net>
2017-10-20 18:52:58 +0530

    add setup script for dotfiles

---
+++ setup.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+figlet "dotfile"
+echo "Author : Mohd Maqbool Alam"
+echo  "Date : 20.10.2017 " 
+
+# ack
+
+ln -sf `pwd`/ack/ackrc ~/.ackrc
+printf "\033[01;31m######################"
+
+#fonts
+
+ln -sf `pwd`/fonts ~/.fonts 
+cd ~
+fc-cache 
+cd - > /dev/null #Hack : it writes previous directory to standard out(1)
+printf "\033[0;32m######################" 
+
+#zsh
+
+ln -sf `pwd`/zsh/.zshrc ~/.zshrc
+printf "\033[0;35m######################\n"
+
+
+
+
+
