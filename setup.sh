413aa57946f952e213ffd28575947398a27fbba4
Mohd Maqbool Alam <maqbool@maqbool.net>
2017-10-20 19:07:18 +0530

    update progress

--- setup.sh
+++ setup.sh
@@ -7,7 +7,7 @@ echo  "Date : 20.10.2017 "
 # ack
 
 ln -sf `pwd`/ack/ackrc ~/.ackrc
-printf "\033[01;31m######################"
+printf "\033[01;31m######################\n"
 
 #fonts
 
@@ -15,12 +15,12 @@ ln -sf `pwd`/fonts ~/.fonts
 cd ~
 fc-cache 
 cd - > /dev/null #Hack : it writes previous directory to standard out(1)
-printf "\033[0;32m######################" 
+printf "\033[0;32m############################################\n" 
 
 #zsh
 
 ln -sf `pwd`/zsh/.zshrc ~/.zshrc
-printf "\033[0;35m######################\n"
+printf "\033[0;35m##################################################################\n"
 
 
 
